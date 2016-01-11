372bb29d330e8f83958104ee743d010c51b88cac
Alex Poloz <alexander.polozkov@gmail.com>
2016-01-11 17:49:25 +0300

    Added algorithm for common prefix

---
+++ java/LongestCommonPrefixFinder.java
@@ -0,0 +1,29 @@
+/**
+ * Created by alpoloz on 11.1.16.
+ */
+public class LongestCommonPrefixFinder {
+
+    public static void main(String... args) {
+        String[] data = {"aaa", "aaab", "aacb", "aa"};
+        System.out.println(findCommonPrefix(data));
+
+    }
+
+    private static String findCommonPrefix(String[] data) {
+        String shortestStr = data[0];
+        for(String str : data) {
+            if (shortestStr.length() > str.length()) {
+                shortestStr = str;
+            }
+        }
+
+        String commonPrefix = shortestStr;
+        for(String str : data) {
+            int i = 0;
+            for(;i < commonPrefix.length() && commonPrefix.charAt(i) == str.charAt(i); i++) {
+            }
+            commonPrefix = str.substring(0, i);
+        }
+        return commonPrefix;
+    }
+}
